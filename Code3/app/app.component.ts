f1a691b2bb6ff85400d57710bf7799c94b02e6a4
amit4alljava <amit4alljava@gmail.com>
2017-12-13 15:08:18 +0530

    Codes

---
+++ Code3/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ANGULAR WORLD';
+  name:string;  // Instance Variable
+  counter:number=0;
+  flag:boolean= false;
+  myclass = "";
+  lastname="";
+  price:number=0;
+  qt:number=0;
+  constructor(){
+  this.name = "";
+  }
+  increment(){
+  this.counter++;
+  if(this.counter>10){
+    this.myclass="red";
+    this.flag = true;
+  }
+  }
+  takeName(event){
+    let start = "Mr ";
+    this.name = start +event.target.value;
+  }
+}
